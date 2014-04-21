11d60aad4de04b071e47d493d1a87d07df51e1e6
Max Tang <max.tang@gmail.com>
2014-04-21 14:39:37 +0800

    implemented the formulas from ex4.pdf & submitted

--- mlclass-ex4-005/mlclass-ex4/nnCostFunction.m
+++ mlclass-ex4-005/mlclass-ex4/nnCostFunction.m
@@ -28,8 +28,16 @@ m = size(X, 1);
 % You need to return the following variables correctly 
 J = 0;
 Theta1_grad = zeros(size(Theta1));
+%fprintf('size of Theta1_grad .\n');
+%size(Theta1_grad )
 Theta2_grad = zeros(size(Theta2));
+%fprintf('size of Theta2_grad .\n');
+%size(Theta2_grad )
 
+%         Note: The vector y passed into the function is a vector of labels
+%               containing values from 1..K. You need to map this vector into a 
+%               binary vector of 1's and 0's to be used with the neural network
+%               cost function.
 % need to recode y, which is of 5000 * 1 with value 1 to 10, to 5000 * 10
 y_recoded = zeros(size(y, 1), size(unique(y), 2));
 labels = unique(y)';
@@ -50,7 +58,11 @@ end;
 % Add ones to the X data matrix for the bias node,
 % which doesn't depend on any of the input value X
 a1 = [ones(m, 1) X];
+	%fprintf('size of a1.\n');
+	%size(a1)
 a2 = [ones(m, 1) sigmoid(a1*Theta1')];
+	%fprintf('size of a2.\n');
+	%size(a2)
 h = sigmoid(a2*Theta2');
 % h is now a matrix of 5000 * 10, w/ each column containing the prob of the given label
 
@@ -75,6 +87,61 @@ J = sum(sum(-y_recoded.*log(h) - (1-y_recoded).*log(1-h), 2))/m ...
 %         Hint: We recommend implementing backpropagation using a for-loop
 %               over the training examples if you are implementing it for the 
 %               first time.
+for t=1:m,
+	delta3 = h(t, :) - y_recoded(t, :);
+	%fprintf('size of delta3.\n');
+	%size(delta3);
+	Theta2_grad	= Theta2_grad + delta3'*a2(t, :);
+
+	delta2 = (delta3 * Theta2);
+	delta2 = delta2(2:end) .* sigmoidGradient(a1(t, :)*Theta1');
+	Theta1_grad	= Theta1_grad + delta2'*a1(t, :);
+	%fprintf('size of delta2.\n');
+	%size(delta2);
+end
+
+Theta2_grad	= Theta2_grad/m;
+Theta1_grad	= Theta1_grad/m;
+%
+% WHAT DO WE WANT?
+% WE WANT dCost/dTheta2 and dCost/dTheta1 for gradient descent
+%
+%
+% dCost/dTheta2 = dCost/dzL3 * dzL3/dTheta2
+%
+% 1st term
+% http://ufldl.stanford.edu/wiki/index.php/Backpropagation_Algorithm
+% Cost	= 0.5*( h - y )^2
+%		= 0.5*( g(zL3) - y )^2	where g = 1/(1+e^-z) with dg/dz = g*(1-g)
+% this is the cost for one observation.  will need to divide by m for multiple
+% dCost/dzL3	= dCost/dg * dg/dzL3
+% 				= ( g - y ) * g*(1-g)
+%dCost_dzL3	= (h - y_recoded) .* h .* (ones(size(h,1), size(h,2)) - h)
+%fprintf('size of dCost_dzL3.\n');
+%size(dCost_dzL3)
+
+% 2nd term
+% zL3	= Theta2 * aL2
+% dzL3/dTheta2 = aL2
+%
+% So, 
+% dCost/dTheta2	= dCost/dzL3 * dzL3/dTheta2
+% 				= (g-y)*g*(1-g) * aL2	where g = g(zL3) = h = aL3
+%
+%
+% dCost/dTheta1 is a bit more tricky
+% dCost/dTheta1 = dCost/dzL2 * dzL2/dTheta1
+%
+% 1st term
+% dCost/dzL2	= dCost/dzL3 * dzL3/dzL2
+% zL3	= Theta2 * aL2
+%		= Theta2 * g(zL2)
+% dzL3/dzL2	= dzL3/dg(zL2) * dg(zL2)/dzL2
+%			= Theta2 * g*(1-g)	where g = g(zL2)
+% dCost/dzL2	= dCost/dzL3 * dzL3/dzL2
+% 				= dCost/dzL3 * Theta2 * g*(1-g)
+% 				= [dCost/dzL3 * Theta2] * g'(zL2)
+% note that in [dCost/dzL3 * Theta2], dCost/dzL3 is the "error term" from next layer and we back propagate it by the means of Theta2 with the weighted average
 %
 % Part 3: Implement regularization with the cost function and gradients.
 %
@@ -87,6 +154,8 @@ J = sum(sum(-y_recoded.*log(h) - (1-y_recoded).*log(1-h), 2))/m ...
 
 
 
+Theta2_grad	= Theta2_grad + [zeros(size(Theta2, 1), 1), Theta2(:, 2:end)]*lambda/m;
+Theta1_grad	= Theta1_grad + [zeros(size(Theta1, 1), 1), Theta1(:, 2:end)]*lambda/m;
 
 
 
